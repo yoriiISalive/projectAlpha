20b0fb97393b4e5c5c8702d1066eac29ca2259bf
yoriiISalive <abhirajmukadam82@gmail.com>
2025-06-17 19:27:08 +0530

    increased the limit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ search_btn.addEventListener('click', async () => {
         return
     }else {
         const key = 'RuJTeKhXAyF9G-9VlCJJv1NlEMuCgGCDgIS-2Brjv3E'
-        const url = `https://api.unsplash.com/search/photos?page=1&query=${search_value}&client_id=${key}`
+        const url = `https://api.unsplash.com/search/photos?per_page=30&query=${search_value}&client_id=${key}`
         let data = await fetch(url)
         data = await data.json()
         console.log(data,"data")
